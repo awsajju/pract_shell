0c0f3cc1ab78fe9b9918815caa2bd2599f1779e5
awsajju <aws.devops.123aws@gmail.com>
2024-11-05 08:31:55 +0530

    comments

---
+++ 04-comments.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# we ahve three comments.
+#1, single line comment
+Number1=$1
+Numbere2=$2
+
+read number1 and number2
+echo "enter $Number1 and $Number2" #2, Inline comment. ---> user is updating the numbers
+
+
+#3, multiline comment.
+
+<<comment
+kjbvkfbbsbck
+zkjfvnkdfj
+nzlfjzvnfeld
+
+comment
+
